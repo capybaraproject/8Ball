9b5d33a9b256cfb82c6f7103f1468a20c0aff659
backaligth <antoine.soros@orange.fr>
2018-12-02 00:11:45 +0100

    work in progress on add custom list .

--- app/src/main/java/app/capy/a8ball/activities/AnswersAddingActivity.java
+++ app/src/main/java/app/capy/a8ball/activities/AnswersAddingActivity.java
@@ -3,7 +3,9 @@ package app.capy.a8ball.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -13,8 +15,20 @@ import java.util.List;
 import app.capy.a8ball.R;
 
 public class AnswersAddingActivity extends AppCompatActivity {
-    EditText etAnswer;
-    ListView lvAnswers;
+   private EditText etAnswer;
+    private Button btAdd;
+    private Button btUpdate;
+    private ListView lvAnswers;
+  public int itemIndex=0;
+
+    public ListView getLvAnswers() {
+        return lvAnswers;
+    }
+
+    public void setLvAnswers(ListView lvAnswers) {
+        this.lvAnswers = lvAnswers;
+    }
+
     ArrayList<String> listAnswer =new ArrayList<>();
     ArrayAdapter<String> adapter;
 
@@ -23,11 +37,23 @@ public class AnswersAddingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answers_adding);
+        btUpdate = findViewById(R.id.btUpdate);
+        btAdd= findViewById(R.id.btAdd);
         lvAnswers=findViewById(R.id.lvAnswers);
         etAnswer = findViewById(R.id.etAddAnswer);
         adapter =   new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,listAnswer);
         lvAnswers.setAdapter(adapter);
+        lvAnswers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                etAnswer.setText(lvAnswers.getItemAtPosition(position).toString());
+            itemIndex=position;
+             ;}
+        });
     }
+
 
 
 
@@ -37,14 +63,18 @@ public class AnswersAddingActivity extends AppCompatActivity {
 
         String userAnswer = etAnswer.getText().toString();
         listAnswer.add(userAnswer);
-        adapter.add(userAnswer);
-     
+
+
         etAnswer.setText("");
-
-
-
 
 
     }
 
+
+    public void updateMessage(View view) {
+        if(!etAnswer.getText().toString().contentEquals("")){
+        
+      listAnswer.set(itemIndex,etAnswer.getText().toString());
+       }
+    }
 }
---
+++ app/src/main/java/app/capy/a8ball/service/SaveListService.java
@@ -0,0 +1,13 @@
+package app.capy.a8ball.service;
+
+
+public class SaveListService {
+
+
+
+
+
+
+        }
+
+
